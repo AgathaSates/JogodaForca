c24b9374a6e0f15f3dc128ae232833239512ae8b
Agatha Sates <satesagatha@gmail.com>
2025-03-18 03:32:47 -0300

    Classe validador

---
+++ JogodaForca.ConsoleApp/Validator.cs
@@ -0,0 +1,35 @@
+﻿namespace JogodaForca.ConsoleApp
+{
+    class Validator
+    {
+        public static int IsaOption(int min, int max)
+        {
+            int option;
+            while (!int.TryParse(Console.ReadLine(), out option) || option < min || option > max)
+            {
+                Write.WrongOption();
+            }
+            return option;
+        }
+
+        public static char IsaChar()
+        {
+            char letter;
+            while (!char.TryParse(Console.ReadLine(), out letter) || !char.IsLetter(letter))
+            {
+                Write.NotaLetter();
+            }
+            return char.ToUpper(letter);
+        }
+
+        public static string YesorNo(string option)
+        {
+            while (string.IsNullOrWhiteSpace(option) || option.ToUpper() != "S" && option.ToUpper() != "N")
+            {
+                Write.WrongOption(); 
+                option = Console.ReadLine()!;
+            }
+            return option.ToUpper();
+        }
+    }
+}
